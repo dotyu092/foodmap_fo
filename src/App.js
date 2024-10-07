6cc489ec5361cb442dc06916d5d6d17afffa4e39
dotyu092 <wjddb0125@gmail.com>
2024-10-07 15:23:05 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+    const [message, setMessage] = useState("")
+
+    useEffect(()=>{
+       fetch("/api/test")
+           .then(res => res.text())
+           .then(m=>setMessage(m))
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
